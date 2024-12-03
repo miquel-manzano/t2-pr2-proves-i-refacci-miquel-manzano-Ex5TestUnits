baa835e9922f5c242c05543be4a7b906ad310861
miquel manzano <miquel.manzano.7e6@itb.cat>
2024-12-03 19:27:18 +0100

    Mensajes a usuario como constantes. Close #2

--- Ex4Refactoring/Program.cs
+++ Ex4Refactoring/Program.cs
@@ -2,29 +2,40 @@
 
 public class Program
 {
+    const string MsgWidthRetangle = "Introdueix l'amplada del rectangle:";
+    const string MsgHeightRetangle = "Introdueix l'altura del rectangle:";
+    const string MsgAreaResult = "L'àrea del rectangle és: ";
+
+    const string MsgRadiusCircle = "Introdueix el radi del cercle:";
+    const string MsgCircleResult = "La circumferència del cercle és: ";
+
+    const string MsgAreaIsGreater = "L'àrea és més gran de 50";
+    const string MsgAreaIsEqual = "L'àrea és entre 20 i 50";
+    const string MsgAreaIsLower = "L'àrea és menor o igual a 20";
+
     public static void Main(string[] args)
     {
         // Sol·licita l'entrada de l'usuari per calcular l'àrea d'un rectangle
-        Console.WriteLine("Introdueix l'amplada del rectangle:");
+        Console.WriteLine(MsgWidthRetangle);
         double width = Convert.ToDouble(Console.ReadLine());
-        Console.WriteLine("Introdueix l'altura del rectangle:");
+        Console.WriteLine(MsgHeightRetangle);
         double height = Convert.ToDouble(Console.ReadLine());
 
         // Calcula l'àrea d'un rectangle
         double area = CalculateArea(width, height);
-        Console.WriteLine("L'àrea del rectangle és: " + area);
+        Console.WriteLine(MsgAreaResult + area);
 
 
         // Sol·licita l'entrada de l'usuari per calcular la circumferència d'un cercle
-        Console.WriteLine("Introdueix el radi del cercle:");
+        Console.WriteLine(MsgRadiusCircle);
         double radius = Convert.ToDouble(Console.ReadLine());
 
         // Calcula circumferència d'un cercle
         double circumference = CalculateCircumference(radius);
-        Console.WriteLine("La circumferència del cercle és: " + circumference);
+        Console.WriteLine(MsgCircleResult + circumference);
 
         // Imprimeix un missatge basat en el valor de l'àrea
-        PrintMsgArea(area); 
+        PrintMsgArea(area);
     }
 
 
@@ -43,15 +54,15 @@ public class Program
     {
         if (area > 50)
         {
-            Console.WriteLine("L'àrea és més gran de 50");
+            Console.WriteLine(MsgAreaIsGreater);
         }
         else if (area > 20)
         {
-            Console.WriteLine("L'àrea és entre 20 i 50");
+            Console.WriteLine(MsgAreaIsEqual);
         }
         else
         {
-            Console.WriteLine("L'àrea és menor o igual a 20");
+            Console.WriteLine(MsgAreaIsLower);
         }
     }
 
