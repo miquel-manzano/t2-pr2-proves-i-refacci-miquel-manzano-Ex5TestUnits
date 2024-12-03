793c1787767f18f29333ae2b98ad16616cbf1824
miquel manzano <miquel.manzano.7e6@itb.cat>
2024-12-03 22:49:24 +0100

    Creacio de Solucio

---
+++ Ex5UnitTests/UnitTest1.cs
@@ -0,0 +1,21 @@
+using ExerciseFive;
+namespace Ex5UnitTests
+{
+    [TestClass]
+    public class AgeRange
+    {
+        [TestMethod]
+        public void AgeInRangeIsTrue(int age)
+        {
+            //Arrange (^-^)
+            bool IsTrue;
+
+            // Act
+            ExerciseFive.PersonaHelper
+
+            // Assert
+
+
+        }
+    }
+}
